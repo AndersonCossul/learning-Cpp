fffc93ce09f6f9d18e705ca4256f262f07e67ceb
Anderson Cossul <anderson@dexdigital.com.br>
2017-11-15 01:05:21 -0200

    Chapter 1

---
+++ Chapter1/io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int readNumber()
+{
+    std::cout << "Enter a number: ";
+    int num;
+    std::cin >> num;
+    return num;
+}
+
+void writeAnswer(int answer)
+{
+    std::cout << "The answer is " << answer;
+}
